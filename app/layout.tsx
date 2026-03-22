1b5d2e985c5d498a81a4dd2b5ca8613b77d4f299
h.yesayan <yesayanhaykaz2@gmail.com>
2026-03-22 12:51:00 +0400

    fav

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export const metadata: Metadata = {
     icons: {
     icon: [
       { url: "/favicon.ico" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" }
     ],
@@ -134,12 +135,8 @@ export default function RootLayout({
           }}
         />
 
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 
-<link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/site.webmanifest" />
 
 <meta name="theme-color" content="#7c3aed" />
       </head>
