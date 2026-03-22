c1dbc674e54a7946fe56a47051882965331b23fa
h.yesayan <yesayanhaykaz2@gmail.com>
2026-03-22 12:46:02 +0400

    fav

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,18 @@ const mono = JetBrains_Mono({
   display: "swap",
 });
 
+
 /* ---------------- SEO Metadata ---------------- */
 
 export const metadata: Metadata = {
+    icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" }
+    ],
+    apple: "/apple-touch-icon.png",
+  },
   metadataBase: new URL(
     process.env.NEXT_PUBLIC_SITE_URL || "https://proxiesseller.cc"
   ),
